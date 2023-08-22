c6c7d063847a23ccda7bd74190e5168fc19cb20e
ItchyDev <119466798+ItchyDev@users.noreply.github.com>
2023-08-21 20:17:34 -0500

    I learned some basic DOM manipulation so now the game has buttons 🙂

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,7 +1,12 @@
+
 //predetermined player choice 
 let playerOption = ""
 let win = 0;
 let loss = 0;
+let draws = 0;
+
+
+document.getElementById("score").textContent = "The score is " + win +" wins, "+ loss +" losses and " + draws + " draws";
 
 
     let options = ["rock","paper","scissors"]
@@ -21,12 +26,16 @@ if(computerChoice === 1){
 
 
 
-function playRound(){
-    //gets player option. 
-    playerOption = prompt("Choose rock, paper or scissors. If not spelled correctly the program will not reconize it and the round will repeat");
-    playerOption = playerOption.toLowerCase();
-console.log(playerOption)
-    
+function playRound(playerOption){
+    //interprets player option into a string
+    if(playerOption === 1){
+        playerOption = "rock"
+    }else if(playerOption === 2){
+        playerOption = "paper"
+    }else if(playerOption === 3){
+        playerOption = "scissors"
+    }
+   console.log(playerOption)
 //gets computers answer
     computerChoice = Math.floor(Math.random() * options.length) + 1;
 //compairs that number and assigns it a choice 
@@ -43,6 +52,7 @@ console.log(`The computer has chosen ${computerChoice}`)
 //compairs 2 answers to find the winner
     if(computerChoice === "rock" && playerOption === "rock"){
         console.log("This match is a draw.")
+        draws++
     }else if(computerChoice === "rock" && playerOption === "paper"){
         console.log("Player Wins!")
         win++
@@ -51,6 +61,7 @@ console.log(`The computer has chosen ${computerChoice}`)
         loss++
     }else if(computerChoice === "paper" && playerOption === "paper"){
         console.log("This match is a draw.")
+        draws++
     }else if(computerChoice === "paper" && playerOption === "scissors"){
         console.log("Player Wins!")
         win++
@@ -59,6 +70,7 @@ console.log(`The computer has chosen ${computerChoice}`)
         loss++
     }else if(computerChoice === "scissors" && playerOption === "scissors"){
         console.log("This match is a draw.")
+        draws++
     }else if(computerChoice === "scissors" && playerOption === "rock"){
         console.log("Player Wins!")
         win++
@@ -69,11 +81,14 @@ console.log(`The computer has chosen ${computerChoice}`)
 
     console.log(win + " wins")
     console.log(loss+ " losses")
+    document.getElementById("score").textContent = "The score is " + win +" wins, "+ loss +" losses and " + draws + " draws";
+    document.getElementById("computer").textContent = "The AI chose "+computerChoice + " and you chose " + playerOption;
+
 }
 
-playRound();
 
 
+/*
 while(win < 5 && loss < 5){
     playRound();
 }
@@ -83,4 +98,4 @@ if(win === 5){
 }else if(loss === 5){
     console.log("You have lost!")
 }
-
+*/
